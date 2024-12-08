03279a624a7165caa7272880fda2f75858ec3a29
unknown <nguyendangduymanh2003@gmail.com>
2024-12-08 09:12:41 +0700

    First initialization UI design

--- app/src/main/java/com/example/javaiotapp/AccountFragment.java
+++ app/src/main/java/com/example/javaiotapp/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.example.javaiotapp;
 
+import android.icu.text.Edits;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +70,41 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
+        ListView list_view = (ListView) view.findViewById(R.id.listview);
+
+        String[] items_user_infor_list = {"Name", "Date of Birth","Address","Phone Number"};
+
+        UserInformation User = new UserInformation();
+        User.setName("Nguyen Dang Duy Manh");
+        User.setAddress("407 - B5");
+        User.setDate_of_birth("11 / 12 / 2003");
+        User.setPhone_number("0832073486");
+
+        User.setName("Manh Nguyen");
+        LinkedHashMap<String, String> User_map = new LinkedHashMap<>();
+
+        User_map.put(items_user_infor_list[0], User.getName());
+        User_map.put(items_user_infor_list[1], User.getDate_of_birth());
+        User_map.put(items_user_infor_list[2], User.getAddress());
+        User_map.put(items_user_infor_list[3], User.getPhone_number());
+
+        List<HashMap<String, String>> User_List = new ArrayList<>();
+
+        SimpleAdapter user_adapter = new SimpleAdapter(this.getContext(), User_List, R.layout.display_user_information_list,
+                new String[]{"First line", "Second line"},
+                new int[]{R.id.User_infor_list1, R.id.User_infor_list2});
+
+        Iterator it = User_map.entrySet().iterator();
+
+        while (it.hasNext()) {
+            HashMap<String, String> User_map_result = new HashMap<>();
+            Map.Entry pair = (Map.Entry)it.next();
+            User_map_result.put("First line", pair.getKey().toString());
+            User_map_result.put("Second line", pair.getValue().toString());
+            User_List.add(User_map_result);
+        }
+        list_view.setAdapter(user_adapter);
+        return view;
     }
 }
